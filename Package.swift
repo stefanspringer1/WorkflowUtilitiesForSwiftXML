839dcbd0d2a16d585472dfb8559c0b424ece6bec
Stefan Springer <springer@stefanspringer.com>
2022-08-19 11:53:53 +0200

    removed auto-created test

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/stefanspringer1/SwiftXML", from: "1.2.158"),
-        .package(url: "https://github.com/stefanspringer1/SwiftWorkflow", from: "0.0.21"),
+        .package(url: "https://github.com/stefanspringer1/SwiftXML", from: "1.2.159"),
+        .package(url: "https://github.com/stefanspringer1/SwiftWorkflow", from: "0.0.22"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
