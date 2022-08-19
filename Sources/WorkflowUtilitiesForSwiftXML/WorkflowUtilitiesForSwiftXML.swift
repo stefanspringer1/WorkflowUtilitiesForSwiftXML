da497ab53857c548335efb919868d230dcf4fc19
Stefan Springer <springer@stefanspringer.com>
2022-08-19 11:20:50 +0200

    extracted common function

--- Sources/WorkflowUtilitiesForSwiftXML/WorkflowUtilitiesForSwiftXML.swift
+++ Sources/WorkflowUtilitiesForSwiftXML/WorkflowUtilitiesForSwiftXML.swift
@@ -3,10 +3,15 @@ import Foundation
 import SwiftXML
 import Workflow
 
+@inline(__always)
+func positionInfo(forNode node: XNode?) -> String? {
+    return (node as? XElement)?.xpath ?? node?.parent?.xpath
+}
+
 public extension Execution {
     
     func log(_ message: Message, node: XNode?, _ arguments: String...) async -> () {
-        await log(message: message, itemPositionInfo: (node as? XElement)?.xpath ?? node?.parent?.xpath, arguments: arguments)
+        await log(message: message, itemPositionInfo: positionInfo(forNode: node), arguments: arguments)
     }
     
 }
@@ -14,7 +19,7 @@ public extension Execution {
 public extension SynchronousCollectingLogger {
     
     func log(_ message: Message, node: XNode?, _ arguments: String...) -> () {
-        self.log(message: message, itemPositionInfo: (node as? XElement)?.xpath ?? node?.parent?.xpath, arguments: arguments)
+        self.log(message: message, itemPositionInfo: positionInfo(forNode: node), arguments: arguments)
     }
     
 }
