f26b6db256b48757bfbb50a2c0081839325358cc
Stefan Springer <springer@stefanspringer.com>
2022-08-26 11:20:19 +0200

    without platforms entry

--- Package.swift
+++ Package.swift
@@ -5,9 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "WorkflowUtilitiesForSwiftXML",
-    platforms: [
-        .macOS(.v10_15),
-    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -17,7 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/stefanspringer1/SwiftXML", from: "1.2.162"),
-        .package(url: "https://github.com/stefanspringer1/SwiftWorkflow", from: "0.0.25"),
+        .package(url: "https://github.com/stefanspringer1/SwiftWorkflow", from: "0.0.31"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
