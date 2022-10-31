5c300c7b690d186595ec987c5edc43a957c959cb
Stefan Springer <springer@stefanspringer.com>
2022-10-31 13:33:20 +0100

    removed „@available(macOS 10.15, *)“

--- Sources/WorkflowUtilitiesForSwiftXML/WorkflowUtilitiesForSwiftXML.swift
+++ Sources/WorkflowUtilitiesForSwiftXML/WorkflowUtilitiesForSwiftXML.swift
@@ -8,7 +8,6 @@ func positionInfo(forNode node: XNode?) -> String? {
     return (node as? XElement)?.xpath ?? node?.parent?.xpath
 }
 
-@available(macOS 10.15, *)
 public extension Execution {
     
     func log(_ message: Message, node: XNode?, _ arguments: String...) -> () {
