784b43e36a7616b5c52e565328b6584a23ac8cde
stefanspringer1 <stefan.springer@icloud.com>
2024-01-08 21:42:44 +0100

    using SwiftXML >= 1.2.370

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/stefanspringer1/SwiftXML", from: "1.2.369"),
+        .package(url: "https://github.com/stefanspringer1/SwiftXML", from: "1.2.370"),
         .package(url: "https://github.com/stefanspringer1/SwiftWorkflow", from: "1.2.202"),
     ],
     targets: [
