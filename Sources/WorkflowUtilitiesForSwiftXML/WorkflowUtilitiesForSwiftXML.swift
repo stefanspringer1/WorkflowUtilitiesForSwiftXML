798c25814b01073c191006c03d582bc7c6fc368a
stefanspringer1 <stefan.springer@icloud.com>
2025-04-26 11:30:40 +0200

    new: logAndUseInfo

--- Sources/WorkflowUtilitiesForSwiftXML/WorkflowUtilitiesForSwiftXML.swift
+++ Sources/WorkflowUtilitiesForSwiftXML/WorkflowUtilitiesForSwiftXML.swift
@@ -10,8 +10,12 @@ func positionInfo(forNode node: XNode?) -> String? {
 
 public extension Execution {
     
-    func log(_ message: Message, node: XNode?, _ arguments: String...) -> () {
+    func log(_ message: Message, node: XNode?, _ arguments: String...) {
         log(message, itemPositionInfo: positionInfo(forNode: node)?.appending(((node as? XText)?.parent ?? node)?.description.prepending(" (").appending(")")), withArguments: arguments)
+    }
+    
+    func logAndUseInfo(_ message: Message, node: XNode?, _ arguments: String...) -> String {
+        logAndUseInfo(message, itemPositionInfo: positionInfo(forNode: node)?.appending(((node as? XText)?.parent ?? node)?.description.prepending(" (").appending(")")), withArguments: arguments)
     }
     
 }
