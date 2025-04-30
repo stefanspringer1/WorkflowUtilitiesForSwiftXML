6de51e7ef74ce538d4abf5dbcb615cce1d890c83
stefanspringer1 <stefan.springer@icloud.com>
2025-04-30 10:41:48 +0200

    using an attachment "xpath"

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # WorkflowUtilitiesForSwiftXML
 
 Some definitions useful when working with [SwiftXML](https://github.com/stefanspringer1/SwiftXML) in the context of [SwiftWorkflow](https://github.com/stefanspringer1/SwiftWorkflow). 
+
+The information about the position fo a node first searches for the attachment of name `xpath` up in the tree (including the node itself) before constructing an informatuion based on the current tree.
--- Sources/WorkflowUtilitiesForSwiftXML/WorkflowUtilitiesForSwiftXML.swift
+++ Sources/WorkflowUtilitiesForSwiftXML/WorkflowUtilitiesForSwiftXML.swift
@@ -5,10 +5,13 @@ import Workflow
 
 @inline(__always)
 func positionInfo(forNode node: XNode?) -> String? {
-    return (node as? XElement)?.xPath ?? node?.parent?.xPath
+    (node as? XElement)?.xPath ?? node?.parent?.xPath
 }
 
+/// The information about the position fo a node first searches for the attachment of name `xpath`
+/// up in the tree (including the node itself) before constructing an informatuion based on the current tree.
 func itemPositionInfo(for node: XNode?) -> String? {
+    node?.ancestorsIncludingSelf.map{ $0.attached["xpath"] as? String }.first ??
     positionInfo(forNode: node)?.appending(((node as? XText)?.parent ?? node)?.description.prepending(" (").appending(")"))
 }
 
