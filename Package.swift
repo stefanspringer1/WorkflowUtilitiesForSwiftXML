28ff5749ff0ffc62d6e7da2429e720c1b4d96857
Stefan Springer <stefan.springer@icloud.com>
2026-01-13 15:16:02 +0100

    using SwiftWorkflow 6.0.0

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/swiftxml/SwiftXML.git", from: "2.0.0"),
-        .package(url: "https://github.com/stefanspringer1/SwiftWorkflow", from: "5.0.0"),
+        .package(url: "https://github.com/swiftxml/SwiftXML.git", from: "2.0.9"),
+        .package(url: "https://github.com/stefanspringer1/SwiftWorkflow", from: "6.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
